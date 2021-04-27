d970bc9b22e32d496f86bf11e4933dc8b9c37a6e
Pavel Sevcik <pavel@sevcik.tk>
2021-04-27 22:09:19 +0700

    ground zero

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # passport-missions-metadata-import
+
+## Requirements
+
+- nodejs (recommended/tested on v14)
+
+## Installation
+
+to install run
+
+```sh
+npm install
+```
+
+## Download Metadata
+
+to download metadata run
+
+```sh
+npm start
+```
---
+++ download-metadata.ts
@@ -0,0 +1,38 @@
+import fetch from "node-fetch";
+import pino from "pino";
+import fs from "fs";
+import stream from "stream";
+
+//#region logger config
+const dest = new stream.PassThrough();
+dest.pipe(process.stdout);
+dest.pipe(fs.createWriteStream("pino.log", { flags: "a" }));
+const logger = pino({ level: "info" }, dest);
+//#endregion
+
+const sourceUrl =
+  "https://omosuite.dstudio.earth/api/v0/aql/public/projects-metadata--public?programSlug=GOS";
+const destFolder = "data";
+
+if (!fs.existsSync(destFolder)) {
+  fs.mkdirSync(destFolder);
+}
+
+(async () => {
+  let data = await fetch(sourceUrl).then((res) => res.json());
+  data.forEach((m) => {
+    fs.writeFileSync(
+      `${destFolder}/${m.thumbnail__filename}`,
+      m.thumbnail__string
+    );
+    fs.writeFileSync(
+      `${destFolder}/${m.transcript__filename}`,
+      m.transcript__string
+    );
+  });
+  let payload = data.map((m) => {
+    let { name, parent_mission_name, metadata, cta } = m;
+    return { name, parent_mission_name, metadata, cta };
+  });
+  fs.writeFileSync(`${destFolder}/_data.json`, JSON.stringify(payload));
+})();
